86d3c124608756723299653713c797990aaba31f
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-10 13:23:48 +0530

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (typeof string !== 'string' || string.length <= 0)
+        return '';
 
+    let reversedString = '';
+    for (let i = 0; i < string.length; i++) {
+        reversedString += string.at(-1 - i);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
