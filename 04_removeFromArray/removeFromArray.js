c0a67bf57f0deb4734261e2704f3cf9c87deb22f
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-10 13:47:18 +0530

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    const array = arguments[0];
+    let filteredArray = array;
+    
+    for (let i = 1; i < arguments.length; i++)
+        filteredArray = filteredArray.filter((elem) => elem !== arguments[i]);
+
+    return filteredArray;
 };
 
 // Do not edit below this line
