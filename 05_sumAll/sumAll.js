3a7f3cf972cc524d33820c8420328ef1144fa169
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-10 13:58:05 +0530

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (typeof number1 !== 'number' ||
+        typeof number2 !== 'number' ||
+        Math.round(number1) !== number1 ||
+        Math.round(number2) !== number2 ||
+        number1 < 0 ||
+        number2 < 0
+    ) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    let lowerLimit = number1, upperLimit = number2;
+    if (number1 > number2) {
+        lowerLimit = number2;
+        upperLimit = number1;
+    }
+
+    for (; lowerLimit <= upperLimit; lowerLimit++)
+        sum += lowerLimit;
+    return sum;
 };
 
 // Do not edit below this line
