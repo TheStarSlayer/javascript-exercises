48e395766fd0a67194647c268df0d238afe9c03a
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-10 12:50:24 +0530

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, noOfRepeats) {
+    if (noOfRepeats < 0)
+        return 'ERROR';
+    let repeatedString = '';
+    for (let i = 0; i < noOfRepeats; i++) {
+        repeatedString += string;
+    }
 
+    return repeatedString;
 };
 
 // Do not edit below this line
