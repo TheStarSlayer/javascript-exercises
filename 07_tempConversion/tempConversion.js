b72b0a03a76b6720c69cea5ccc579912f492cc05
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-10 14:19:44 +0530

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let tempInCelsius = (fahrenheit - 32) * (5/9);
+  return validTemp(tempInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let tempInFahrenheit = (celsius * (9/5)) + 32;
+  return validTemp(tempInFahrenheit);
 };
+
+const validTemp = function(magnitude) {
+  if (Math.round(magnitude) === magnitude)
+    return magnitude
+  return parseFloat(magnitude.toFixed(1));
+}
 
 // Do not edit below this line
 module.exports = {
