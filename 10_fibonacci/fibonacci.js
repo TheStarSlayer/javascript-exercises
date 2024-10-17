5d43a7acbbca43d7432a070bd9e4632c25d5cafd
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-17 15:51:06 +0530

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciIndex) {
+    if (typeof fibonacciIndex === 'string')    
+        fibonacciIndex = Number(fibonacciIndex);
 
+    if (fibonacciIndex < 0)
+        return 'OOPS';
+
+    if (!fibonacciIndex)
+        return 0;
+
+    let prevFibonacci = 0, fibonacci = 1;
+    let currentFibonacciIndex = 1;
+
+    while (currentFibonacciIndex < fibonacciIndex) {
+        let temp = fibonacci;
+        fibonacci = fibonacci + prevFibonacci;
+        prevFibonacci = temp;
+        currentFibonacciIndex++;
+    }
+    return fibonacci;
 };
 
 // Do not edit below this line
