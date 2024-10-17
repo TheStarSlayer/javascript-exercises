6e31fa81d75d6febbe6fa99517161e42fc1c3125
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-17 16:28:03 +0530

    Add solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if (!death)
+        death = (new Date()).getFullYear();
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((maxAgePerson, currentPerson) => {
+        maxAge = getAge(maxAgePerson.yearOfBirth, maxAgePerson.yearOfDeath);
+        currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return maxAge < currentAge ? currentPerson : maxAgePerson;
+    })
 };
 
 // Do not edit below this line
