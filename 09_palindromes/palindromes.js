6e1dede87df01f62e9ead152db5c10b259b2e681
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2024-10-17 15:40:27 +0530

    Add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const alphanumerics = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const processedTextArray = text
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerics.includes(character));
 
+    let processedTextReverse = processedTextArray.toReversed().join('');
+    processedText = processedTextArray.join('');
+
+    return processedText === processedTextReverse;
 };
 
 // Do not edit below this line
